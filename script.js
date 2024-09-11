a52d1cd32fd8829f438bd026b7bd41ff65bceac9
Freyjaa8 <83167912+Freyjaa8@users.noreply.github.com>
2024-09-11 15:23:08 +0200

    js + css

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ async function utilisateurs ()
         const fichiers = await fetch( 'champions.json' );
         const data = await fichiers.json();
         const randomIndex = Math.floor( Math.random() * data[ 0 ].length );
-        console.log( data[0].length );
         const randomUser = data[0][ randomIndex ];
-        document.getElementById( "texte1" ).innerHTML = `Tu joueras : ${ randomUser.name } <div><img src="${randomUser.icon}"><div>`;
+        document.getElementById( "texte1" ).innerHTML = `Tu joueras : ${ randomUser.name } <div><img src="${ randomUser.icon }"><div>`;
+        document.getElementById( "destiny" ).classList.add( "destiny" );
     }
 
     catch ( error )
@@ -30,14 +30,21 @@ async function roles ()
         const randomIndex = Math.floor( Math.random() * data[1].length );
         const randomUser = data[1][ randomIndex ];
         document.getElementById( "texte2" ).innerHTML = `Ton role : ${ randomUser.Role }`;
+        document.getElementById( "destiny" ).classList.add( "destiny" );
     }
 
     catch ( error )
     {
         alert( 'Bébou, ca ne fonctionne pas, oups', error );
     }
-} 
+}
 
+function wow ()
+{
+    let phrases = [ "Bonne Chance avec ce pick Bébou <3", "Ptdrrr c'est de la merde", "Bébouuuu Bisouuuus <3", "Vasy on va encore perdre", "Bébou t'es le meilleur <3", "Je sais pas si on va casser des culs avec ça..." ]
+    let random = phrases[ Math.floor( Math.random() * phrases.length ) ] ;
+    document.querySelector( "h1" ).innerHTML = `<h2>${ random }</h2>`;
+}
 const boutonR = document.getElementById( "role" );
 boutonR.addEventListener( 'click', roles );
 
@@ -45,6 +52,7 @@ function total ()
 {
     utilisateurs();
     roles();
+    wow();
 }
 const boutonT = document.getElementById( "total" );
 
